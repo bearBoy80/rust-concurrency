a994c042a865d8e7bb6f586fd11ee89db146fc3b
bearboy80 <2373319771@qq.com>
2024-04-30 18:47:29 +0800

    add thread matrix multiply

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,7 +1,8 @@
 use core::fmt;
-use std::ops::{Add, AddAssign, Mul};
+use std::{ops::{Add, AddAssign, Mul}, sync::mpsc, thread};
 
-use anyhow::Ok;
+
+use crate::{dot_product, Vector};
 
 // 定义一个矩阵
 #[derive(Debug)]
@@ -10,21 +11,85 @@ pub struct Matrix<T: fmt::Debug> {
     row: usize, // 行
     col: usize, //列
 }
+pub struct MsgInput<T>{
+    idx: usize,
+    row:Vector<T>,
+    col:Vector<T>,
+}
+impl<T> MsgInput<T> {
+    fn new(idx: usize, row: Vector<T>, col: Vector<T>) -> Self {
+        Self { idx, row, col }
+    }
+}
+pub struct MsgOutput<T>{
+    idx: usize,
+    value :T,
+}
+
+pub struct Msg<T>{
+    input :MsgInput<T>,
+    sender :oneshot::Sender<MsgOutput<T>>,
+}
+impl <T> Msg<T>  {
+    fn new(input: MsgInput<T>, sender: oneshot::Sender<MsgOutput<T>>)->Self{
+        Self{input,sender}
+    }
+}
 pub fn multiply<T>(a: &Matrix<T>, b: &Matrix<T>) -> anyhow::Result<Matrix<T>>
 where
-    T: Default + Copy + Add<Output = T> + AddAssign + Mul<Output = T> + fmt::Debug,
+    T: Default + Copy + Add<Output = T> + AddAssign + Mul<Output = T> + fmt::Debug + Send + 'static,
 {
     if a.col != b.row {
         return Err(anyhow::anyhow!("matrix multi error: a.col != b.row"));
     }
-    // a = 1,2, 3,4  2,2 b =1,2,3,4 2,2
-    let mut data: Vec<T> = vec![T::default(); a.row * b.col];
+    const NUM_THREADS: usize =4;
+    //构造sender
+    let senders = (0..NUM_THREADS)
+    .map(|_| {
+        let (tx, rx) = mpsc::channel::<Msg<T>>();
+        thread::spawn(move || {
+            for msg in rx {
+                let value = dot_product(msg.input.row, msg.input.col)?;
+                if let Err(e) = msg.sender.send(MsgOutput {
+                    idx: msg.input.idx,
+                    value,
+                }) {
+                    eprintln!("Send error: {:?}", e);
+                }
+            }
+            Ok::<_,anyhow::Error >(())
+        });
+        tx
+    })
+    .collect::<Vec<_>>();
+    // 
+    let matrix_len = a.row * b.col;
+    let mut receivers = Vec::with_capacity(matrix_len);
+    // 多线程去做点乘
+    let mut data: Vec<T> = vec![T::default();matrix_len];
     for i in 0..a.row {
         for j in 0..b.col {
-            for k in 0..a.col {
-                data[i * b.col + j] += a.data[i * a.col + k] * b.data[k * b.col + j]
+            let row = Vector::new(&a.data[i * a.col..(i + 1) * a.col]);
+            let col_data = b.data[j..]
+                .iter()
+                .step_by(b.col)
+                .copied()
+                .collect::<Vec<_>>();
+            let col = Vector::new(col_data);
+            let idx = i * b.col + j;
+            let input = MsgInput::new(idx, row, col);
+            let (tx, rx) = oneshot::channel();
+            let msg = Msg::new(input, tx);
+            if let Err(e) = senders[idx % NUM_THREADS].send(msg) {
+                eprintln!("Send error: {:?}", e);
             }
+            receivers.push(rx);
         }
+    }
+      // map/reduce: reduce phase
+    for rx in receivers {
+        let output = rx.recv()?;
+        data[output.idx] = output.value;
     }
     let result = Matrix::new(data, a.row, b.col);
     Ok(result)
@@ -51,5 +116,12 @@ mod tests {
         assert!(c.row == a.row);
         assert!(c.col == b.col);
         assert_eq!(c.data, vec![22, 28, 49, 64])
+    }
+    #[test]
+    fn test_mult_matrix_by2() {
+        let a: Matrix<i32> = Matrix::new([1, 2, 3, 4, 5, 6,7,8,9,10,11,12], 3, 4);
+        let b: Matrix<i32> = Matrix::new([1, 2, 3, 4, 5, 6,7,8], 4, 2);
+        let c = multiply(&a, &b).unwrap();
+        assert_eq!(c.data,vec![50, 60, 114, 140, 178, 220]);
     }
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,21 +1,32 @@
 use anyhow::Result;
 use std::ops::{Add, AddAssign, Deref, Mul};
 
+#[derive(Debug)]
 pub struct Vector<T> {
     data: Vec<T>,
 }
 pub fn dot_product<T>(a: Vector<T>, b: Vector<T>) -> Result<T>
-where T: Copy + Default + Add<Output = T> + Mul<Output = T> + AddAssign<T>,
+where
+    T: Copy + Default + Add<Output = T> + Mul<Output = T> + AddAssign<T>,
 {
     if a.len() != b.len() {
-       return Err(anyhow::anyhow!("Dot product error: a.len != b.len"))
+        return Err(anyhow::anyhow!("Dot product error: a.len != b.len"));
     }
-    Err(anyhow::anyhow!(""))
+    let mut sum = T::default();
+    for i in 0..a.len() {
+        sum += a[i] * b[i];
+    }
+    Ok(sum)
 }
 
-impl<T> Deref for Vector<T>{
+impl<T> Deref for Vector<T> {
     type Target = Vec<T>;
     fn deref(&self) -> &Self::Target {
         &self.data
+    }
+}
+impl<T> Vector<T> {
+    pub fn new(data: impl Into<Vec<T>>) -> Self {
+        Self { data: data.into() }
     }
 }
